6a308b79123c10e13e217e3083c00307e44e860a
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-03-25 15:20:38 +0000

    lier la modele Ticket avec User et Response

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -7,6 +7,33 @@ use Illuminate\Database\Eloquent\Model;
 class Ticket extends Model
 {
     protected $fillable = [
-        'subject', 'description', 'customer_id', 'assigned_agent_id', 'status', 'claimed_at', 'resolved_at'
+        'subject',
+        'description',
+        'customer_id',
+        'assigned_agent_id',
+        'status',
+        'claimed_at',
+        'resolved_at'
     ];
+
+    public function customer()
+    {
+        return $this->belongsTo(User::class, 'customer_id');
+    }
+
+    public function assignedAgent()
+    {
+        return $this->belongsTo(User::class, 'assigned_agent_id')
+            ->select(['id', 'name', 'email']);
+    }
+
+    public function responses()
+    {
+        return $this->hasMany(Response::class);
+    }
+
+    public function claimingAgent()
+    {
+        return $this->belongsTo(User::class, 'assigned_agent_id');
+    }
 }
