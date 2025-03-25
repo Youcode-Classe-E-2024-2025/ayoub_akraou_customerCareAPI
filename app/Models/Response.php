e2b2506bcafff16b843e9b614b8c5c8aeb5a4cc6
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-03-25 15:22:10 +0000

    creer les relations entre Response et Ticket et User

--- app/Models/Response.php
+++ app/Models/Response.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Response extends Model
 {
     protected $fillable = ['content', 'ticket_id', 'agent_id'];
+
+    public function ticket()
+    {
+        return $this->belongsTo(Ticket::class);
+    }
+
+    public function agent()
+    {
+        return $this->belongsTo(User::class, 'agent_id');
+    }
 }
