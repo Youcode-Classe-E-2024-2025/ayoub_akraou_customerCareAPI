6dbd74e68101a622cd11fecdca3e907f379dda43
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-03-26 10:12:13 +0000

    implementer la fonctionnalité de register dans AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Response;
+use App\Repositories\ResponseRepository;
+use App\Repositories\TicketRepository;
+use App\Services\TicketService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +15,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(TicketRepository::class, function ($app) {
+            return new TicketRepository($app->make(\App\Models\Ticket::class));
+        });
+    
+        $this->app->bind(TicketService::class, function ($app) {
+            return new TicketService($app->make(TicketRepository::class));
+        });
+
+        $this->app->bind(ResponseRepository::class, function ($app) {
+            return new ResponseRepository($app->make(Response::class));
+        });
     }
 
     /**
