01aafba43077dc56a20e0d7a36be12ccf5632f40
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-03-26 10:54:34 +0000

    ajouter les routes de ticket

--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,10 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
+    Route::apiResource('tickets', TicketController::class)
+        ->where(['ticket' => '[0-9]+']);
+
+    Route::get('/tickets/available', [TicketController::class, 'availableTickets']);
+    Route::post('/tickets/{ticket}/claim', [TicketController::class, 'claim']);
+    Route::patch('/tickets/{ticket}/resolve', [TicketController::class, 'resolve']);
+
